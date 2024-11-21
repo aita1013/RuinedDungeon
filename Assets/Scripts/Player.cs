e6443ddc41fc5a034032bbc4854ade4295dc920b
aita1013 <gangbulantailang@gmail.com>
2024-11-21 20:46:53 +0900

    ジャンプ・走りを追加してアニメーションを付けた

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
-    public Rigidbody2D rd;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpSpeed;
+    private Rigidbody2D rd;
     private bool Grounded;
-
+    private Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
         rd = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
@@ -19,26 +25,38 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.D))
         {
-            transform.localScale = new Vector3(1.7f,1.7f,1.7f);
-            transform.position += new Vector3(0.1f,0,0);
+            transform.localScale = new Vector3(2f,2f,2f); //3Dの場合反転する必要があるから大きさを変換する処理を入れている(28要参照）
+        
+            transform.position += new Vector3(moveSpeed,0,0);
+            animator.SetBool("isRunning",true);
         }
-
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("isRunning",false);
+        }
         if(Input.GetKey(KeyCode.A))
         {
-            transform.localScale = new Vector3(-1.7f,1.7f,1.7f);
-            transform.position += new Vector3(-0.1f,0,0);
+            transform.localScale = new Vector3(-2f,2f,2f);
+            transform.position += new Vector3(-moveSpeed,0,0);
+            animator.SetBool("isRunning",true);
         }      
-
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded == true)
+        else if(Input.GetKeyUp(KeyCode.A))
         {
-            rd.AddForce(Vector2.up * 500);
+            animator.SetBool("isRunning",false);
         }
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded == true)    //ジャンプは地面についているときじゃないとできないため、
+        {
+            rd.AddForce(Vector2.up * jumpSpeed);
+            animator.SetBool("isJumping",true);
+        }
+       
        }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Ground")
         {
-            Grounded = true;
+            Grounded = true;    //地面についているときにtrue（ジャンプ可能）
+            animator.SetBool("isJumping",false);
         }
     
     }
@@ -48,7 +66,7 @@ public class Player : MonoBehaviour
     {
         if(collision.gameObject.tag =="Ground")
         {
-            Grounded = false;
+            Grounded = false;   //地面から離れているときにfalse(ジャンプ不可)にする
         }
     }
 
